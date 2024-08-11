d97733e39038e094e14c06b887635291d56548c9
TechSunny <sunnymotivation2@gmail.com>
2024-08-11 21:31:01 +0530

    volume control coded, used seet alert for msg

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@ let title = document.getElementById("title");
 let fileSize = document.getElementById("size");
 
 
-
-
 uploadInput.onchange = (e)=>{
     const inputFile = e.target;
     var song = inputFile.files[0];
@@ -13,16 +11,18 @@ uploadInput.onchange = (e)=>{
     title.innerHTML = songFile;
     fileSize.innerHTML = "Size: " + (song.size/1000000).toFixed(1) + " "+ "mb";
 
-    // creating audio element
-
-    let audioTag = document.createElement("audio");
-    let songUrl = URL.createObjectURL(song );
-    audioTag.src = songUrl;
-    audioTag.play();
-
     // select icons & buttons
     let playBtn = document.getElementById("play-btn")
     let playIcon = document.getElementById("play-icon");
+
+    
+    // creating audio element
+    let audioTag = document.createElement("audio");
+    let songUrl = URL.createObjectURL(song);
+    audioTag.src = songUrl;
+    audioTag.play();
+    playIcon.className = "ri-pause-fill";
+
 
     //  play & pause
     playBtn.onclick =()=>{
@@ -34,9 +34,8 @@ uploadInput.onchange = (e)=>{
             playIcon.className = "ri-play-fill";
             audioTag.pause();
         }
-        
     }
-
+        
     // mute-unmute
     let muteBtn = document.getElementById("mute-btn");
     let muteIcon = document.getElementById("mute-icon");
@@ -53,7 +52,7 @@ uploadInput.onchange = (e)=>{
             }
                     }
 
-     //  looping
+     //  looping audio
      let loopBtn = document.getElementById("loop-btn");
      let loopIcon =document.getElementById("loop-icon");
 
@@ -83,15 +82,64 @@ uploadInput.onchange = (e)=>{
         }
     }
 
-    // duration show
+    // get duration of audio
     let fullDuration = document.getElementById("full-duration");
-
     audioTag.onloadedmetadata=()=>{
         let length = audioTag.duration;
-        let minute = length/60;
-        fullDuration.innerHTML = length;
+        let minute =Math.floor(length/60);
+        let second = Math.round(length%60);
+        fullDuration.innerHTML = minute + "m" + second +"s";
     }
 
+
+    //  show progress & current time of audio
+    
+    audioTag.ontimeupdate=()=>{
+        let currentDuration = document.getElementById("current-duration");
+        let currentTime = audioTag.currentTime;
+        let minute =Math.floor(currentTime/60);
+        let second = Math.round(currentTime%60);
+        currentDuration.innerHTML = minute + "m" + second +"s";
+        let progressBar = document.getElementById("progress-bar");
+        let totalDuration = audioTag.duration;
+        let percentDuration = Math.round((currentTime/totalDuration)*100) ;
+        progressBar.style.width = percentDuration + "%";    
+    }
+
+    // volume control
+    let volumeInput = document.getElementById("volume");
+    volumeInput.oninput=()=>{
+        audioTag.volume = volumeInput.value; 
+    }
+
+    // show volume slider
+    let volumeSlider = document.getElementById("volume-box");
+    let volumeBtn = document.getElementById("volume-btn");
+    volumeBtn.onclick=()=>{
+        if(volumeSlider.style.display == "none"){
+            volumeSlider.style.display = "block";   
+        }
+        else{
+            volumeSlider.style.display = "none";
+        }
+    }
+
+}
+
+
+ // when no audio selected
+ let audioTag =null;
+ let playBtn = document.getElementById("play-btn");
+ playBtn.onclick= ()=>{
+    console.log(audioTag);
+    
+    if(audioTag==null){
+        Swal.fire({
+        icon: "error",
+        title: "Sorry!",
+        text: "no audio selected",
+      });
+    }
 }
 
 
